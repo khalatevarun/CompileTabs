ff2e89604d7b69580b91fb75a356d4d1f1a80995
Varun Khalate <khalatevarun@gmail.com>
2021-12-14 20:35:20 +0530

    update: data structure to store the links to enable grouping feature

--- index.js
+++ index.js
@@ -1,47 +1,67 @@
-let myLeads = [];
+let myTabs = [
+  {
+    name: 'Favourite',
+    links: [
+      'https://roc8.my.stacker.app/your-profile#Cohort%20Submissions',
+      'https://roc8.my.stacker.app/your-profile#Cohort%20Submissions',
+    ],
+  },
+];
+let currentGroup = 0; // index that points to current group object
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
-const ulEl = document.getElementById('ul-el');
+const contentEl = document.getElementById('content-el');
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myTabs'));
 const deleteBtn = document.getElementById('delete-btn');
 const tabBtn = document.getElementById('tab-btn');
+const currentEl = document.getElementById('current-el');
+
+currentEl.innerHTML = myTabs[currentGroup].name;
 
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
+  myTabs = leadsFromLocalStorage;
+  render(myTabs);
 }
 
 tabBtn.addEventListener('click', function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem('myTabs', JSON.stringify(myLeads));
-    render(myLeads);
+    myTabs[currentGroup].links.push(tabs[0].url);
+    localStorage.setItem('myTabs', JSON.stringify(myTabs));
+    render(myTabs);
   });
 });
 
-deleteBtn.addEventListener('dblclick', function () {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
-});
+// deleteBtn.addEventListener('dblclick', function () {
+//   localStorage.clear();
+//   myTabs = [];
+//   render(myTabs);
+// });
+
+render(myTabs);
 
 inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value);
+  myTabs.push(inputEl.value);
   inputEl.value = '';
-  localStorage.setItem('myTabs', JSON.stringify(myLeads));
-  render(myLeads);
+  localStorage.setItem('myTabs', JSON.stringify(myTabs));
+  render(myTabs);
 });
 
-function render(leads) {
+function render(groups) {
   let listItems = '';
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `;
+
+  for (let i = 0; i < groups.length; i++) {
+    console.log(groups);
+    listItems += `<h3>${groups[i].name}</h3><ul id="ul-el">`;
+    for (let j = 0; j < groups[i].links.length; j++) {
+      listItems += `
+      <li>
+          <a target='_blank' href='${groups[i].links[j]}'>
+              ${groups[i].links[j]}
+          </a>
+      </li>
+  `;
+    }
+    listItems += '</ul>';
   }
-  ulEl.innerHTML = listItems;
+  contentEl.innerHTML = listItems;
 }
