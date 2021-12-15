85421fe18dc2576c6ad3974e4760cb43ca393488
Varun Khalate <khalatevarun@gmail.com>
2021-12-16 00:20:21 +0530

    add: input validation to check if input is a valid url

--- index.js
+++ index.js
@@ -34,22 +34,34 @@ tabBtn.addEventListener('click', function () {
 });
 
 deleteEl.addEventListener('dblclick', function () {
-  let updatedTabs = myTabs.filter(
-    (tab) => tab.name != myTabs[currentGroup].name
-  );
+  myTabs = myTabs.filter((tab) => tab.name != myTabs[currentGroup].name);
   currentGroup = 0;
-  currentEl.innerHTML = updatedTabs[currentGroup].name;
-  localStorage.setItem('myTabs', JSON.stringify(updatedTabs));
-  render(updatedTabs);
+  currentEl.innerHTML = myTabs[currentGroup].name;
+  localStorage.setItem('myTabs', JSON.stringify(myTabs));
+  render(myTabs);
 });
+
+function is_url(str) {
+  regexp =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(str)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 render(myTabs);
 
 inputBtn.addEventListener('click', function () {
-  myTabs[currentGroup].links.push(inputEl.value);
-  inputEl.value = '';
-  localStorage.setItem('myTabs', JSON.stringify(myTabs));
-  render(myTabs);
+  if (is_url(inputEl.value)) {
+    myTabs[currentGroup].links.push(inputEl.value);
+    inputEl.value = '';
+    localStorage.setItem('myTabs', JSON.stringify(myTabs));
+    render(myTabs);
+  } else {
+    //display error saying that the url is not valid.
+  }
 });
 
 newBtn.addEventListener('click', function () {
@@ -113,6 +125,14 @@ document.addEventListener('click', function (e) {
 
 function render(groups) {
   let listItems = '';
+
+  console.log(groups);
+
+  if (groups.length == 1) {
+    deleteEl.style.display = 'none';
+  } else {
+    deleteEl.style.display = 'inline';
+  }
 
   for (let i = 0; i < groups.length; i++) {
     console.log(groups);
